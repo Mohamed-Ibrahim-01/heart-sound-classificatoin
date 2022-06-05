14808c3d1cceb32273ebd1c565f299c8694c9fe1
mohamed-ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2022-06-05 14:50:01 +0200

    code outline

---
+++ feature_extraction.py
@@ -0,0 +1,21 @@
+#orginal sound file [-----------------------------------]
+#segmented [--------][--------][-------][---------] 
+
+def build_features_df(segments):
+    pass
+
+def extract_features(segment):
+    pass
+
+# load_pascal [[array1, label][array2, label]]
+
+# build_segments : [[segment, label] [ segment2, label]]
+    # segment_array()
+
+# bulid features : build dataframe of features
+    # extract_features()
+
+# explore
+
+#model
+ 
---
+++ utils.py
@@ -0,0 +1,3 @@
+# return sounds np 2D array (with labels) [[array1, label][array2, label]]
+def load_pascal()
+    pass
