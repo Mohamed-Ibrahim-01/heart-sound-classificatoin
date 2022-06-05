08be3392a3efd98a8d70064373ac690547c4aad7
NadaElmasry <57152677+NadaElmasry@users.noreply.github.com>
2022-06-05 13:18:28 +0200

    stat features and mfcc

--- features.py
+++ features.py
@@ -8,8 +8,12 @@ def stat_features(array):
     std= np.std(array)
     kurtosis = scipy.stats.kurtosis(array, axis=0, bias=True)
     skewness = scipy.stats.skew(array, axis=0, bias=True)
+    iqr = scipy.stats.iqr(array)
+    first_percentile = np.percentile(array,25)
+    second_percentile = np.percentile(array,50)
+    third_percentile = np.percentile(array,75)
 
-    return (mean,median,std,kurtosis,skewness)
+    return (mean,median,std,kurtosis,skewness,iqr,first_percentile,second_percentile,third_percentile)
 
 
 def mfcc(array,sr=4000):
